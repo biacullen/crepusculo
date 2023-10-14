d8423dc7e78add3686be6fed161ab56e0d7a6c23
biacullen <biancajesusdesouzaa@gmail.com>
2023-10-14 17:52:46 -0300

    update crepusculo rev.5

---
+++ js/script_menu.js
@@ -0,0 +1,20 @@
+// script responsável pela automação do menu hamburger
+
+const btnMobile = document.getElementById('btn-mobile');
+// const >>declara a nossa variável, no uso do decorrer do código
+
+functiontoggleMenu(Event) {
+    if (Event.type === 'touchstart') Event.prevenDefault ();
+    const nav = document.getElementById ('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    Event.currenTarget.setAttribute ('aria-expanded', active);
+    if (active){
+        Event.currenTarget.setAttribute('aria-label', 'fechar Menu');
+    }else{
+        Event.currenTarget.setAttribute*('aria-label','abir Menu');
+        }    
+    }
+
+    btnMobile.addEventListener('click',toggleMenu)
+    btnMobile;addEventListener('touchstart',toggleMenu)
